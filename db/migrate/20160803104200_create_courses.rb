4abe9e378a22fc8b1dbe02fdfd89810d165aaa7e
wowdyaln <rogersmk@gmail.com.tw>
2016-08-03 19:40:37 +0800

    [Model] 建立 courses table

--- db/migrate/20160803104200_create_courses.rb
+++ db/migrate/20160803104200_create_courses.rb
@@ -1,6 +1,11 @@
 class CreateCourses < ActiveRecord::Migration
   def change
     create_table :courses do |t|
+      t.string :title
+      t.text :description
+      t.date :open_date
+      t.integer :cost_chip
+
 
       t.timestamps null: false
     end
