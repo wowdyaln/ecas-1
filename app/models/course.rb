43a8519cd203eb92a3b9beb54bfe032387b8221f
wowdyaln <rogersmk@gmail.com.tw>
2016-08-05 15:24:53 +0800

    14. 資料驗證： title ; description 不能空白

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,8 @@
 class Course < ActiveRecord::Base
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :open_date, presence: true
+  validates :cost_chip, presence: true
+
 end
