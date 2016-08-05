e16b7b17184e892c2e138c997bc9ad827b21b9f8
wowdyaln <rogersmk@gmail.com.tw>
2016-08-05 15:11:55 +0800

    10. course/create 完成

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -10,8 +10,27 @@ class CoursesController < ApplicationController
   end
 
 
+  def new
+    @course = Course.new
+  end
 
 
+  def create
+    @course = Course.create(course_params)
+
+    if @course.save
+      redirect_to courses_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def course_params
+  params.require(:course).permit(:title, :description, :open_date, :cost_chip)
+  end
 
 
 end
