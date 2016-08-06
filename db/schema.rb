1054fef92d2f1f4d4cb05eff0fc16f76affccd9c
wowdyaln <rogersmk@gmail.com.tw>
2016-08-06 17:52:21 +0800

    23. rake db:migrate 。建立 course_users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160806070610) do
+ActiveRecord::Schema.define(version: 20160806094611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "course_users", force: :cascade do |t|
+    t.integer  "course_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "courses", force: :cascade do |t|
     t.string   "title"
