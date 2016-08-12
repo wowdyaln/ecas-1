c4d56a4fe1e13948380f9524a3cd63be074be11d
s20a3264 <s20a3264@hotmail.com>
2016-08-12 18:57:16 +0800

    小修calendar 與 helper

--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -1,6 +1,18 @@
 module CalendarHelper
-	def query_date(date, cell)
-		{ date: {year: date.year, month: date.month, day: cell["day"]} }
+	def query_date(date, day)
+		query_date = Date.new(date.year, date.month, day)
+		{ date: query_date }
+	end
+
+	def render_calendar
+		case @calendar_type
+		when "admin"
+			render 'calendar/admin_calendar'
+		when "user"
+			render 'calendar/user_calendar'
+		when "basic"
+			render 'calendar/basic_calendar'
+		end	
 	end
 
 	def render_calendar
--- app/helpers/course_helper.rb
+++ app/helpers/course_helper.rb
@@ -1,8 +1,8 @@
 module CourseHelper
   #生成發表課程的預設日期
-  def generate_date(hash)
-  	if hash
-  		Time.new(hash[:year], hash[:month], hash[:day]).to_date
+  def generate_default_date(date)
+  	if date
+  		date.to_date
   	else
   		Date.tomorrow
   	end		
