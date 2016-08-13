538d925aa2a12b355e35a35f3a064899df464d39
s20a3264 <s20a3264@hotmail.com>
2016-08-13 18:45:00 +0800

    修改預約課程清單頁面

--- app/helpers/course_helper.rb
+++ app/helpers/course_helper.rb
@@ -7,4 +7,12 @@ module CourseHelper
   		Date.tomorrow
   	end		
   end	
+
+  def render_reserve_btn(course)
+  	if current_user
+  		link_to("預約", reserve_course_path(course), class: "btn-1")
+  	else
+  		content_tag(:span, "預約需登入", class: "btn-disable")
+  	end	
+  end
 end
